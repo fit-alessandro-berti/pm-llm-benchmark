5ded9593392990310246a49fdfa050ffdf055735
Alessandro Berti <alessandro.berti@fit.fraunhofer.de>
2024-09-13 08:01:53 +0200

    updated guided generation script

--- utils/guided_generation.py
+++ utils/guided_generation.py
@@ -16,12 +16,19 @@ for q in questions:
 
     answer_path = os.path.join(answers_folder, model_name.replace("/", "").replace(":", "") + "_" + q).replace(".png", ".txt")
 
-    pyperclip.copy(question)
+    proceed = False
 
-    if os.path.exists(answer_path):
-        continue
+    if not os.path.exists(answer_path):
+        proceed = True
     else:
+        contents = open(answer_path, "r").read().strip()
+        if not contents:
+            proceed = True
+
+    if proceed:
+        pyperclip.copy(question)
+
         F = open(answer_path, "w")
         F.close()
 
-    subprocess.run(["notepad.exe", answer_path])
+        subprocess.run(["notepad.exe", answer_path])
