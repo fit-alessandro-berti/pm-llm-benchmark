3f4991d987752b9e5b66edc9084a3a8a6aea5bc9
Alessandro Berti <alessandro.berti@fit.fraunhofer.de>
2024-10-28 11:19:15 +0100

    added support for Google Gemini

--- answer.py
+++ answer.py
@@ -51,16 +51,22 @@ def dump_response(response, target_file):
         print("response dumping failed")
 
 
-def query_text_simple(question_path, complete_url, target_file, callback):
-    question = open(question_path, "r", encoding="utf-8").read()
+def query_text_simple_generic(question, api_url, target_file):
+    complete_url = api_url + "chat/completions"
+
     messages = [{"role": "user", "content": question}]
+
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {API_KEY}"
+    }
 
     payload = {
         "model": MODEL_NAME,
         "messages": messages,
     }
-
     dump_payload(payload, target_file)
+
     response = requests.post(complete_url, headers=headers, json=payload).json()
     dump_response(response, target_file)
 
@@ -68,11 +74,51 @@ def query_text_simple(question_path, complete_url, target_file, callback):
         response_message = response["choices"][0]["message"]["content"]
     except Exception as e:
         raise Exception(str(response))
+
+    return response_message
+
+
+def query_text_simple_google(question, api_url, target_file):
+    complete_url = api_url + "models/" + MODEL_NAME + ":generateContent?key=" + API_KEY
+
+    headers = {
+        "Content-Type": "application/json",
+    }
+
+    payload = {
+        "contents": [
+            {"parts": [
+                {"text": question}
+            ]}
+        ]
+    }
+    dump_payload(payload, target_file)
+
+    response = requests.post(complete_url, headers=headers, json=payload).json()
+    dump_response(response, target_file)
+
+    try:
+        response_message = response["candidates"][0]["content"]["parts"][0]["text"]
+    except Exception as e:
+        raise Exception(str(response))
+
+    return response_message
+
+
+def query_text_simple(question_path, api_url, target_file, callback):
+    question = open(question_path, "r", encoding="utf-8").read()
+
+    if "googleapis" in api_url:
+        response_message = query_text_simple_google(question, api_url, target_file)
+    else:
+        response_message = query_text_simple_generic(question, api_url, target_file)
+
     callback(response_message, target_file)
 
 
-def query_image_simple(question_path, complete_url, target_file, callback):
-    base64_image = encode_image(question_path)
+def query_image_simple_generic(base64_image, api_url, target_file):
+    complete_url = api_url + "chat/completions"
+
     messages = [{"role": "user", "content": [{"type": "text", "text": "Can you describe the provided visualization?"},
                                              {"type": "image_url",
                                               "image_url": {"url": f"data:image/png;base64,{base64_image}"}}]}]
@@ -83,6 +129,11 @@ def query_image_simple(question_path, complete_url, target_file, callback):
         "max_tokens": 4096,
     }
 
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {API_KEY}"
+    }
+
     response = requests.post(complete_url, headers=headers, json=payload).json()
     dump_response(response, target_file)
 
@@ -91,6 +142,48 @@ def query_image_simple(question_path, complete_url, target_file, callback):
     except Exception as e:
         print(response)
         raise Exception(e)
+
+    return response_message
+
+
+def query_image_simple_google(base64_image, api_url, target_file):
+    complete_url = api_url + "models/" + MODEL_NAME + ":generateContent?key=" + API_KEY
+
+    headers = {
+        "Content-Type": "application/json",
+    }
+
+    payload = {
+        "contents": [
+            {"parts": [
+                {"text": "Can you describe the provided visualization?"},
+                {"inline_data": {
+                    "mime_type": "image/png",
+                    "data": base64_image
+                }}
+            ]}
+        ]
+    }
+
+    response = requests.post(complete_url, headers=headers, json=payload).json()
+    dump_response(response, target_file)
+
+    try:
+        response_message = response["candidates"][0]["content"]["parts"][0]["text"]
+    except Exception as e:
+        raise Exception(str(response))
+
+    return response_message
+
+
+def query_image_simple(question_path, api_url, target_file, callback):
+    base64_image = encode_image(question_path)
+
+    if "googleapis" in api_url:
+        response_message = query_image_simple_google(base64_image, api_url, target_file)
+    else:
+        response_message = query_image_simple_generic(base64_image, api_url, target_file)
+
     callback(response_message, target_file)
 
 
@@ -98,14 +191,14 @@ API_URL = "https://api.openai.com/v1/"
 #API_URL = "http://127.0.0.1:11434/v1/"
 #API_URL = "https://api.deepinfra.com/v1/openai/"
 #API_URL = "https://api.mistral.ai/v1/"
+#API_URL = "https://generativelanguage.googleapis.com/v1beta/"
 
-MODEL_NAME = "gpt-4o"
-API_KEY = open("api_key.txt", "r").read()
+MODEL_NAME = "gpt-4o-2024-08-06"
+API_KEY = open("api_key.txt", "r").read().strip()
 
 WAITING_TIME_RETRY = 60
 
 questions = [x for x in os.listdir("questions") if x.endswith(".txt") or x.endswith(".png")]
-complete_url = API_URL + "chat/completions"
 
 for q in questions:
     question_path = os.path.join("questions", q)
@@ -114,19 +207,14 @@ for q in questions:
     if not os.path.exists(answer_path):
         print("Executing", question_path)
 
-        headers = {
-            "Content-Type": "application/json",
-            "Authorization": f"Bearer {API_KEY}"
-        }
-
         while not os.path.exists(answer_path):
             try:
                 if question_path.endswith(".txt"):
-                    query_text_simple(question_path, complete_url, answer_path, callback_write)
+                    query_text_simple(question_path, API_URL, answer_path, callback_write)
                     break
-                elif MODEL_NAME.startswith("pixtral") or MODEL_NAME.startswith("chatgpt-4o") or MODEL_NAME.startswith("gpt-4o") or MODEL_NAME.startswith("gpt-4-turbo") or MODEL_NAME.startswith("gpt-4-vision") or MODEL_NAME.startswith("meta-llama/Llama-3.2-11B") or MODEL_NAME.startswith("meta-llama/Llama-3.2-90B"):
+                elif MODEL_NAME.startswith("pixtral") or MODEL_NAME.startswith("chatgpt-4o") or MODEL_NAME.startswith("gpt-4o") or MODEL_NAME.startswith("gpt-4-turbo") or MODEL_NAME.startswith("gpt-4-vision") or MODEL_NAME.startswith("meta-llama/Llama-3.2-11B") or MODEL_NAME.startswith("meta-llama/Llama-3.2-90B") or MODEL_NAME.startswith("gemini-"):
                     try:
-                        query_image_simple(question_path, complete_url, answer_path, callback_write)
+                        query_image_simple(question_path, API_URL, answer_path, callback_write)
                     except:
                         traceback.print_exc()
                     break
