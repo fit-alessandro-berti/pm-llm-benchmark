1dcb8ee02bd73a54de4d040963c8effe08c6f2d5
Alessandro Berti <alessandro.berti@fit.fraunhofer.de>
2024-11-15 07:49:36 +0100

    refactoring

--- utils/rename_answ_model.py
+++ utils/rename_answ_model.py
@@ -1,13 +1,22 @@
 import os
 
-original_name = "o1-mini"
-novel_name = "o1-mini-2024-09-12"
 
-files = [x for x in os.listdir("../answers") if x.startswith(original_name)]
+def do_renaming(base_path, original_name, novel_name):
+    files = [x for x in os.listdir(base_path) if x.startswith(original_name)]
 
-for f in files:
-    original_path = os.path.join("../answers", f)
-    new_path = original_path.replace(original_name, novel_name)
+    for f in files:
+        original_path = os.path.join(base_path, f)
+        new_path = original_path.replace(original_name, novel_name)
 
-    print(original_path, new_path, os.path.exists(original_path), os.path.exists(new_path))
-    os.rename(original_path, new_path)
+        print(original_path, new_path, os.path.exists(original_path), os.path.exists(new_path))
+        os.rename(original_path, new_path)
+
+
+if __name__ == "__main__":
+    original_name = "Gemini-Pro-Exp-1114"
+    novel_name = "Gemini-Exp-1114"
+
+    do_renaming("../answers", original_name, novel_name)
+    do_renaming("../evaluation", original_name, novel_name)
+
+
