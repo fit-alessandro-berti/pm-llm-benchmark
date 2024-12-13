fb6f7dba9bdcefdb1fbb39f614f86360424aa590
Alessandro Berti <alessandro.berti@fit.fraunhofer.de>
2024-12-13 12:24:37 +0100

    added mistral-small and QwQ-32B-Preview

--- common.py
+++ common.py
@@ -81,7 +81,7 @@ MODELS_DICT = {
             "meta-llama/Llama-3.3-70B-Instruct", "meta-llama/Meta-Llama-3.1-8B-Instruct",
             "meta-llama/Llama-3.2-90B-Vision-Instruct", "meta-llama/Llama-3.2-11B-Vision-Instruct",
             "meta-llama/Llama-3.2-1B-Instruct", "meta-llama/Llama-3.2-3B-Instruct",
-            "nvidia/Llama-3.1-Nemotron-70B-Instruct"
+            "nvidia/Llama-3.1-Nemotron-70B-Instruct", "google/gemma-2-9b-it"
         }
     },
     "ollama_local": {
