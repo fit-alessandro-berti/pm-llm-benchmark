fa72b0f4df8b0365e08791922003ba9dd9687a8c
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2025-02-21 15:46:32 +0100

    minor update

--- common.py
+++ common.py
@@ -10,7 +10,7 @@ import sys
 from typing import Dict, Any
 
 # the model used to respond to the questions
-ANSWERING_MODEL_NAME = "Grok-3-beta-thinking-20250221" if len(sys.argv) < 3 else sys.argv[1]
+ANSWERING_MODEL_NAME = "DeepSeek-R1-Dynamic-Quant" if len(sys.argv) < 3 else sys.argv[1]
 
 # judge model
 EVALUATING_MODEL_NAME = "gpt-4o-2024-11-20" if len(sys.argv) < 3 else sys.argv[2]
@@ -119,7 +119,7 @@ MODELS_DICT = {
 
 def force_custom_evaluation_lrm(answering_model_name):
     model_name = answering_model_name.lower()
-    for p in ["qwq", "qvq", "deepseek-r1-distill", "deepseek-ai", "deepseek-r1-zero", "grok-3-beta-thinking"]:
+    for p in ["qwq", "qvq", "deepseek-r1-distill", "deepseek-ai", "deepseek-r1-zero", "grok-3-beta-thinking", "deepseek-r1-dynamic-quant"]:
         if p in model_name:
             return True
     return False
@@ -280,10 +280,10 @@ def query_text_simple_generic(question, api_url, target_file):
                 chunk = data.get("response", "")
                 response_message += chunk
                 chunk_count += 1
-                #print(chunk_count)
+                print(chunk_count)
 
                 if chunk_count % 10 == 0:
-                    #print(chunk_count, len(response_message), response_message.replace("\n", " ").replace("\r", "").strip())
+                    print(chunk_count, len(response_message), response_message.replace("\n", " ").replace("\r", "").strip())
                     pass
 
     else:
