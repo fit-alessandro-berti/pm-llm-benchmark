15afc06d9c6b869793d2cfc541928c9a6e4d510e
Alessandro Berti <alessandro.berti@fit.fraunhofer.de>
2026-05-04 15:08:19 +0200

    update

--- utils/list_lrms.py
+++ utils/list_lrms.py
@@ -5,34 +5,26 @@ try:
 except ModuleNotFoundError:
     from script_bootstrap import chdir_repo_root
 
-from common import force_custom_evaluation_lrm
-
-
 chdir_repo_root()
 
 
-def read_co(file_path):
-    ret = ""
+def starts_with_think(file_path):
     try:
-        F = open(file_path, "r")
-        ret = F.read()
-        F.close()
-    except:
-        F = open(file_path, "r", encoding="utf-8")
-        ret = F.read()
-        F.close()
-    return ret
+        with open(file_path, "r", encoding="utf-8") as handler:
+            return handler.read(1024).lstrip().startswith("<think>")
+    except UnicodeDecodeError:
+        with open(file_path, "r", encoding="latin-1") as handler:
+            return handler.read(1024).lstrip().startswith("<think>")
 
 
-llms = set(x.split("_")[0] for x in os.listdir("answers"))
-llms = {x.strip() for x in llms if x.strip()}
-llms = {x for x in llms if force_custom_evaluation_lrm(x)}
-llms1 = set()
-for x in llms:
-    answers = [os.path.join("answers", y) for y in os.listdir("answers") if y.startswith(x)]
-    co = read_co(answers[0]).strip()
-    if co.startswith("<"):
-        llms1.add(x)
-F = open(os.path.join("utils", "lrms_list.txt"), "w")
-F.write("\n".join(llms1))
-F.close()
+llms = set()
+for filename in os.listdir("answers"):
+    if "_cat" not in filename:
+        continue
+
+    file_path = os.path.join("answers", filename)
+    if starts_with_think(file_path):
+        llms.add(filename.split("_cat", 1)[0])
+
+with open(os.path.join("utils", "lrms_list.txt"), "w", encoding="utf-8") as handler:
+    handler.write("\n".join(sorted(llms)))
